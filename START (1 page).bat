352f645198146a77972e553f7bee629b4bda9fb8
Ilya Kositsin <ikositsin.99@gmail.com>
2022-09-01 21:13:42 +0400

    added --pages flag for program guide format; filters and whitelist moved to 'settings' directory

---
+++ START (1 page).bat
@@ -0,0 +1,4 @@
+@echo off
+python imlazyaf.py --pages 1
+type "%~dp0homer.txt"
+pause
--- imlazyaf.py
+++ imlazyaf.py
@@ -1,77 +1,55 @@
-"""  I'm lazy af script
-  Basic script for automate editing the program guide
+"""  
+    I'm lazy af script
+    Basic script for automate editing the program guide
+  
+    by Ilya Kositsin
 
-  - rewritten in Python cuz I'm tired of this shi.. I mean PowerShell
-      - reworked 'Delete midnight shows' module, now they got smarter and don't delete morning shows
-      - added sorting channels by day
-      - optimized program guide space by removal consecutive identical shows 
-        and listing the times of the show in one line
-      - few unimportant but nice little things
-  by Ilya Kositsin
-  3 April 2022
-  Special for "Призыв" newspaper
-  tnx to StackOverflow gentlemen and my laziness
- 
+    Special for "Призыв" newspaper
+    tnx to StackOverflow gentlemen and my laziness 
 """
-
-#   Bugfix from 12 april 2022
-#   - Fixed day_sort_guide, now he sorts guide correctly
-#     (not through the ass, may the editor forgive me when I put a broken program guide)
-#   - deleted shit code (not all)
-#   - added nice code
-
-#   Update from 22 april 2022
-#   - settings for editing program guide are now external
-#     (no more hardcoded data)
-
-#   Update from 16 july 2022
-#   - added setting for checking markers
-#         before:
-#          12.34 - premiere! shit show
-#          56.78 - shit show
-#         after:
-#          12.34, 56.78 - shit show
-#   - added setting for checking name of series of the show
-#         before:
-#          12.34 - shit show-2 shit in my ass
-#          56.78 - shit show-2 shit in my pantsu 
-#         after:
-#          12.34, 56.78 - shit show
-#   - added timer for measure the script running time 
 
 __author__ = 'Ilya Kositsin'
 __licence__ = 'MIT'
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 __email__ = 'kositsin2010@yandex.ru'
 __status__ = 'Production'
 
 import os
 import time
+import argparse
 from itertools import groupby
 from collections import deque
 
-def delete_consecutive_identical_shows(program_guide: list) -> list:
-    previous_show = ''
-    edited_program_guide = []
-    
+def delete_markers(show: str) -> str:
     if not hasattr(delete_consecutive_identical_shows, 'useless_markers'):
-        delete_consecutive_identical_shows.useless_markers = load_optimizing_filter('useless_markers')
+        delete_markers.useless_markers = load_optimizing_filter('useless_markers')
+    
+    for marker in delete_markers.useless_markers:
+        if marker in show:
+            return show.replace(marker + ' ', '')
 
-    if not hasattr(delete_consecutive_identical_shows, 'shows_with_names_of_series'):
-        delete_consecutive_identical_shows.shows_with_names_of_series = load_optimizing_filter('shows_with_names_of_series')
+    return show 
 
-    for line in program_guide:
-        time, show = line.split(maxsplit=1)
+def replace_shows_with_names_of_series(show: str) -> str:
+    if not hasattr(replace_shows_with_names_of_series, 'shows_with_names_of_series'):
+            replace_shows_with_names_of_series.shows_with_names_of_series = load_optimizing_filter('shows_with_names_of_series')
 
-        for marker in delete_consecutive_identical_shows.useless_markers:
-            if marker in show:
-                show = show.replace(marker + ' ', '') 
-
-        for show_wnos in delete_consecutive_identical_shows.shows_with_names_of_series:
+    for show_wnos in replace_shows_with_names_of_series.shows_with_names_of_series:
             if show_wnos in show:
                 splt = show.split('"')
                 splt[1] = show_wnos
-                show = str.join('"', splt)
+
+                return str.join('"', splt)
+    
+    return show
+
+def delete_consecutive_identical_shows(program_guide: list) -> list:
+    previous_show = ''
+    edited_program_guide = []  
+
+    for line in program_guide:
+        time, show = line.split(maxsplit=1)
+        show = replace_shows_with_names_of_series(delete_markers(show))        
 
         if show != previous_show:
             edited_program_guide.append(time + ' ' + show)
@@ -79,7 +57,7 @@ def delete_consecutive_identical_shows(program_guide: list) -> list:
     
     return edited_program_guide
 
-def optimize_program_guide(chan_day_program: list) -> list:
+def optimize_program_guide(chan_day_program: list, program_guide_pages_count: int) -> list:
     optimized_guide = []
 
     if not hasattr(optimize_program_guide, 'replacement_rules'):
@@ -111,7 +89,9 @@ def optimize_program_guide(chan_day_program: list) -> list:
 
         optimized_guide.append(line)
 
-    optimized_guide.append('\n')
+    if program_guide_pages_count == 2:
+        optimized_guide.append('\n')
+    
     return optimized_guide
 
 def delete_midnight_shows(chan_day_program: list) -> list:
@@ -133,7 +113,7 @@ def delete_midnight_shows(chan_day_program: list) -> list:
 
     return edited_guide
 
-def day_sort_guide(_guide: list, chans_name: list, weekdays: list) -> list:
+def day_sort_guide(program_guide_pages_count: int, _guide: list, chans_name: list, weekdays: list) -> list:
     _guide = [deque(i) for i in _guide]
     sorted_guide = [[] for i in range(len(weekdays))]
 
@@ -144,7 +124,7 @@ def day_sort_guide(_guide: list, chans_name: list, weekdays: list) -> list:
         _chan = list(filter('\n'.__ne__, _chan))
 
         chan_day_sort = [list(y) for x, y in groupby(_chan, lambda z: day_check(z, weekdays)) if not x]              
-        chan_day_sort = [optimize_program_guide(i) for i in chan_day_sort]
+        chan_day_sort = [optimize_program_guide(i, program_guide_pages_count) for i in chan_day_sort]
        
         for x in chan_day_sort:
             x.insert(0, chans_name[chan_name_index] + '\n')
@@ -185,11 +165,14 @@ def save_guide(_guide: list, _weekdays: list):
         with open(os.path.join(dir_name, str(i + 1) + ' - ' + _weekdays[i] + '.txt'), 'w', encoding='utf-8') as f:
             f.writelines(_guide[i])
 
-def load_chans_list() -> dict:
+def load_chans_list(program_guide_pages_count: int) -> dict:
     chans = dict()
     buffer = []
-    with open('whitelist.txt', 'r', encoding='utf-8-sig') as f:
+    with open(os.path.join('settings', 'whitelist.txt'), 'r', encoding='utf-8-sig') as f:
         buffer = f.readlines()
+
+    if program_guide_pages_count == 1:
+        buffer = buffer[:-1]
 
     for i in buffer:
         chan_name_and_path = i.split(';', maxsplit=1)
@@ -204,7 +187,7 @@ def load_chans_list() -> dict:
 def load_guide_filter() -> dict:
     filtr = dict()
     buffer = []
-    with open('filter.txt', 'r', encoding='utf-8-sig') as f:
+    with open(os.path.join('settings', 'filter.txt'), 'r', encoding='utf-8-sig') as f:
         buffer = f.readlines()
     
     for i in buffer:
@@ -219,7 +202,7 @@ def load_guide_filter() -> dict:
 
 def load_optimizing_filter(name: str) -> list:
     filtr = list()
-    with open(name + '.txt', 'r', encoding='utf-8') as f:
+    with open(os.path.join('settings', name + '.txt'), 'r', encoding='utf-8') as f:
         for line in f:
             filtr.append(line.strip())
     
@@ -227,7 +210,14 @@ def load_optimizing_filter(name: str) -> list:
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    chans = load_chans_list()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pages', action='store', dest='count_of_pages_of_program_guide', type=int, help='Count of pages of program guide (1 or 2)')
+    args = parser.parse_args()
+    if args.count_of_pages_of_program_guide != 1:
+        args.count_of_pages_of_program_guide = 2
+
+    chans = load_chans_list(args.count_of_pages_of_program_guide)
 
     weekdays = [
         'Понедельник',
@@ -239,7 +229,7 @@ if __name__ == "__main__":
         'Воскресенье'
     ]
 
-    guide = day_sort_guide(load_guide(list(chans.values())), chans_name=list(chans.keys()), weekdays=weekdays)
+    guide = day_sort_guide(args.count_of_pages_of_program_guide, load_guide(list(chans.values())), chans_name=list(chans.keys()), weekdays=weekdays)
     save_guide(guide, weekdays)
 
     print(f"Вместо 1.5 часа ты потратил {time.perf_counter() - start:0.4f} секунд")
